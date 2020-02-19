b0af6e1acdc2661334f21fcb30fa2114631be118
Scott Nelson <scott.nelsonjr@gmail.com>
2020-02-19 16:59:03 -0500

    add override arugment to clean logs

--- code/clean_logs.py
+++ code/clean_logs.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import argparse
 
 from file_paths import (
     update_log,
@@ -56,19 +57,21 @@ def clean_processed():
         os.remove(f"{processed_data}\\{filename}")
 
 
-def clean_files():
+def clean_files(override=False):
     """
     Deletes all files in the logs, raw data, and processed data folder
     if the Luigi log indicates that the pipeline ran
     correctly on today's date.
     """
+    override = override is not False
+
     date_string = f"Date: {str(pd.to_datetime('today').date())}True"
 
     file_handle = open(luigi_log, "r")
     line_list = [line.strip("\n") for line in file_handle.readlines()]
     file_handle.close()
 
-    if date_string == line_list[0]:
+    if (date_string == line_list[0]) | override:
         clean_update_logs()
         remove_ehr_files()
         clean_raw()
@@ -78,4 +81,14 @@ def clean_files():
 
 
 if __name__ == "__main__":
-    clean_files()
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--override",
+        default=False,
+        help="True to override database completion for clean up",
+    )
+
+    arguments = parser.parse_args()
+    clean_files(**vars(arguments))
